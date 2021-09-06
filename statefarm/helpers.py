fe9cc13a8bceea6f9e304d108ddafc1676df3c9d
nghitrampham <nghitrampham@gmail.com>
2021-09-05 21:22:10 -0700

    base model

--- statefarm/helpers.py
+++ statefarm/helpers.py
@@ -2,20 +2,6 @@ import numpy as np
 import pandas as pd 
 
 def create_dummy_df(df, cat_cols, dummy_na):
-    '''
-    INPUT:
-    df - pandas dataframe with categorical variables you want to dummy
-    cat_cols - list of strings that are associated with names of the categorical columns
-    dummy_na - Bool holding whether you want to dummy NA vals of categorical columns or not
-    
-    OUTPUT:
-    df - a new dataframe that has the following characteristics:
-            1. contains all columns that were not specified as categorical
-            2. removes all the original columns in cat_cols
-            3. dummy columns for each of the categorical columns in cat_cols
-            4. if dummy_na is True - it also contains dummy co lumns for the NaN values
-            5. Use a prefix of the column name with an underscore (_) for separating 
-    '''
     for col in  cat_cols:
         try:
             # for each cat add dummy var, drop original column
